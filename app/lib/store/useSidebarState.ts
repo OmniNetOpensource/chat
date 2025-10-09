5c04cefe03b71eb1a78759cbe62b6529f1dfe773
Young Mou <skylerloveuk@gmail.com>
2025-10-09 18:06:09 +0800

    few bugs fixed

--- app/lib/store/useSidebarState.ts
+++ app/lib/store/useSidebarState.ts
@@ -1,12 +1,11 @@
-import {create} from 'zustand'
+import { create } from 'zustand';
 
-interface SidebarState{
-    isSidebarOpen:boolean,
-    setSidebar:()=>void,
+interface SidebarState {
+  isSidebarOpen: boolean;
+  setSidebar: () => void;
 }
 
-
-export const useSidebarState =create<SidebarState>((set)=>({
-    isSidebarOpen:false,
-    setSidebar:()=>set((state)=>({isSidebarOpen:!state.isSidebarOpen})),
+export const useSidebarState = create<SidebarState>((set) => ({
+  isSidebarOpen: false,
+  setSidebar: () => set((state) => ({ isSidebarOpen: !state.isSidebarOpen })),
 }));
